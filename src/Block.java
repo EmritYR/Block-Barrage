c446805c64866895fc618880705e1e733ac2e0a0
Emrit Ramharracksingh <eramharracksingh@gmail.com>
2019-09-27 15:37:10 -0400

    resizable to false

--- src/Block.java
+++ src/Block.java
@@ -4,7 +4,7 @@ import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 public class Block {
-    private static final int X_SIZE = 40;
+    private static final int X_SIZE = 140;
     private static final int Y_SIZE = 10;
     private static final int SPEED = 9;
 
