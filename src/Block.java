f39d9401888eaf54f5e16c70af9081fcc614442f
Emrit Ramharracksingh <eramharracksingh@gmail.com>
2019-09-30 14:17:41 -0400

    shareeda's sound code

--- src/Block.java
+++ src/Block.java
@@ -1,6 +1,9 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
 public class Block {
@@ -13,6 +16,8 @@ public class Block {
     private Player player;
     private int x;
     private int y;
+
+    private Clip hitBlockSound = null;
 
     public Block(JPanel p, Player py) {
         panel = p;
@@ -48,6 +53,7 @@ public class Block {
 
         if (playerHitsBlock()) {
             player.decrementLives();
+            playClip(1);
 
             try {
                 Thread.sleep(3);
@@ -55,5 +61,30 @@ public class Block {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void loadClips() {
+        File audioFile = new File("../sounds/swoosh.wav");
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            AudioFormat format = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            hitBlockSound = (Clip) AudioSystem.getLine(info);
+            hitBlockSound.open(audioStream);
+        } catch (UnsupportedAudioFileException uafe) {
+            System.out.println("The specified audio file is not supported.");
+            uafe.printStackTrace();
+        } catch (LineUnavailableException lue) {
+            System.out.println("Audio line for playing back is unavailable.");
+            lue.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("Error playing the audio file.");
+            ioe.printStackTrace();
+        }
+    }
+
+    public void playClip(int index) {
+        if (index == 1 && hitBlockSound != null)
+            hitBlockSound.start();
     }
 }
